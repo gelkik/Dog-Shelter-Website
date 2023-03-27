13105fad77254d284e9e2cd23441100afd30e91b
GuestAlexander <119532414+GuestAlexander@users.noreply.github.com>
2023-03-27 16:40:22 -0400

    Tables Created for Models Folder

--- server/models.py
+++ server/models.py
@@ -1,5 +1,38 @@
 from sqlalchemy_serializer import SerializerMixin
-
+db = sqlalchemy()
 from config import db
 
-# Models go here!
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Coloumn(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    location = db.Column(db.String, primary_key=True)
+
+
+
+class Dog(db.Model):
+    __tablename__ = 'dogs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    breed = db.Column(db.String)
+    age = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    personality = db.Column(db.String)
+    location = db.Column(db.String)
+    likes_other_dogs = db.Column(db.Boolean)
+    likes_cats = db.Column(db.Boolean)
+
+
+
+class Location(db.Model):
+    __tablename__ = 'location'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String)
+    
+
+
+

9410ecb619cd8b19cffd3b4a82983ef170c7631e
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-03-30 16:24:59 -0600

    Add files via upload

---
+++ client/src/pages/DogList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import BreadCumb from '../components/doglists/BreadCumb';
+import AdoptionShop from '../components/AdoptionShop'
+
+
+function DogList() {
+
+
+  return(
+
+	  <main>
+	   <BreadCumb/>
+	   <AdoptionShop/>
+	  </main>
+  )
+}
+
+export default DogList;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import MainSlider from '../components/MainSlider';
+import AdoptionShop from '../components/AdoptionShop'
+
+
+
+
+function Home() {
+
+
+  return (
+
+	
+		  <main>
+				<MainSlider/>
+				<AdoptionShop/>
+				
+		</main>
+	  
+  )
+}
+
+export default Home;
